377f6770fbff7e41cfef7225fc6f74848600bfa2
Kadiresan Ramachandran <32431444+rkrajini@users.noreply.github.com>
2021-08-31 12:51:02 +1000

    Update app.js

--- app.js
+++ app.js
@@ -12,38 +12,38 @@ hbs.registerPartials(__dirname + '/views/partials');
 // view engine setup
 
 
-// otenv.config({ path: './.env'});
+dotenv.config({ path: './.env'});
 
 const app = express();
 
-// const db = mysql.createConnection({
-//   host: process.env.DATABASE_HOST,
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE
-// });
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+});
 
-// app.use(fileUpload());
+app.use(fileUpload());
 
-// const publicDirectory = path.join(__dirname, './public');
-// app.use(express.static(publicDirectory));
-// const uploadDirectory = path.join(__dirname, './upload');
-// app.use(express.static(uploadDirectory));
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+const uploadDirectory = path.join(__dirname, './upload');
+app.use(express.static(uploadDirectory));
 
 // // Parse URL-encoded bodies (as sent by HTML forms)
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // // Parse JSON bodies (as sent by API clients)
-// app.use(express.json());
-// app.use(cookieParser());
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-// hbs.registerHelper('ifCond', function(a, b, opts) {
-//   if (a == b) {
-//       return opts.fn(this)
-//   } else {
-//       return opts.inverse(this)
-//   }
-// });
+app.use(express.json());
+app.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerHelper('ifCond', function(a, b, opts) {
+  if (a == b) {
+      return opts.fn(this)
+  } else {
+      return opts.inverse(this)
+  }
+});
 
 // // db.connect( (error) => {
 // //   if(error) {
